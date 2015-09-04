ad63e89f978ec9881be32d04adf6435caa47cafb
Arthur Corenzan <arthur@corenzan.com>
2015-09-04 00:16:17 -0300

    Include package.bat

--- package.bat
+++ package.bat
@@ -1,46 +1,46 @@
+:: Package your ESO add-on ready for distribution.
+:: Version 1.0 Thu Sep 03 23:09:05 BRT 2015
 @echo off
 setlocal enableextensions enabledelayedexpansion
 
-set zip=C:\Program Files\7-Zip\7z.exe
+set zip=%ProgramFiles%\7-Zip\7z.exe
 if not exist "%zip%" goto :zipnotfound
 
-set name=
 for %%* in (.) do set name=%%~nx*
-echo Packaging: %name%
-
 for /F "tokens=3" %%i in ('findstr /C:"## Version:" %name%.txt') do set version=%%i
-echo Version: %version%
-
-md tmp\%name%
-
-for /F %%i in ('findstr ".lua .xml" %name%.txt') do (
-  set file=%%i
-
-  if !file:$^(language^)^=! == !file! (
-    xcopy /E !file! tmp\%name%
-  ) else (
-    xcopy /C /E /Y !file:$^(language^)=EN! tmp\%name%
-    xcopy /C /E /Y !file:$^(language^)=DE! tmp\%name%
-    xcopy /C /E /Y !file:$^(language^)=FR! tmp\%name%
-  )
-)
-
-exit /B
 
 set archive=%name%-%version%.zip
 
+echo * Packaging %archive%...
+
+md tmp\%name%
+
+for /F %%i in ('findstr /B /R "[^#;]" %name%.txt') do (
+  set file=%%~nxi
+
+  if !file:$^(language^)^=! == !file! (
+    set files=!file! !files!
+  ) else (
+    set files=!file:$^(language^)=EN! !files!
+    set files=!file:$^(language^)=DE! !files!
+    set files=!file:$^(language^)=FR! !files!
+  )
+)
+
+robocopy . tmp\%name% %files%%name%.txt /S /XD tmp .git /NJH /NJS /NFL /NDL > nul
+
 cd tmp
-"%zip%" a -tzip ..\%archive% %name%
+"%zip%" a -tzip -bd ..\%archive% %name% > nul
 cd ..
 rd /S /Q tmp
 
+echo * Done^^!
 echo.
-echo Done
 
 pause
 exit /B
 
 :zipnotfound
-echo 7-Zip cannot be found, get it at http://www.7-zip.org
+echo 7-Zip cannot be found, get it free at http://www.7-zip.org
 pause
 exit /B
