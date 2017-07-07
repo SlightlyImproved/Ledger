5aaa1ed5624c2b963379c5225bd2fe89a5d5c193
Arthur Corenzan <arthur@corenzan.com>
2017-07-06 22:40:07 -0300

    Fix time localization calculation

--- L10n.lua
+++ L10n.lua
@@ -1,14 +1,19 @@
 -- L10n
 -- The MIT License © 2016 Arthur Corenzan
 
-function L10n_GetTimeZone()
-    return GetSecondsSinceMidnight() - (GetTimeStamp() % 86400)
+function L10n_GetTimeZoneShift()
+    local localTimeShift = GetSecondsSinceMidnight() - (GetTimeStamp() % 86400)
+	if localTimeShift < -43200 then
+        return localTimeShift + 86400
+    else
+        return localTimeShift
+    end
 end
 
 function L10n_GetLocalizedDate(timestamp, language)
     language = language or GetCVar("Language.2")
 
-    local string = GetDateStringFromTimestamp(timestamp + L10n_GetTimeZone())
+    local string = GetDateStringFromTimestamp(timestamp + L10n_GetTimeZoneShift())
 
     if language == "en" then
         return string
@@ -19,13 +24,13 @@ end
 
 function L10n_GetLocalizedTime(timestamp, language)
     language = language or GetCVar("Language.2")
-    local precision 
+    local precision
     if (language == "en") then
         precision = TIME_FORMAT_PRECISION_TWELVE_HOUR
     else
         precision = TIME_FORMAT_PRECISION_TWENTY_FOUR_HOUR
     end
-    return FormatTimeSeconds((timestamp + L10n_GetTimeZone()) % 86400, TIME_FORMAT_STYLE_CLOCK_TIME, precision)
+    return FormatTimeSeconds(timestamp % 86400 + L10n_GetTimeZoneShift(), TIME_FORMAT_STYLE_CLOCK_TIME, precision)
 end
 
 function L10n_GetLocalizedDateTime(...)
